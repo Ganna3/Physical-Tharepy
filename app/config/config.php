79016ad89a53baa71bd8151cfbd15a6ffae675af
amr2234 <58447804+amr2234@users.noreply.github.com>
2021-06-11 22:26:55 +0200

    Htaccses , config file name change

--- app/config/config.php
+++ app/config/config.php
@@ -5,7 +5,7 @@
     define('DB_NAME', "clinic_web_app");
     define('APPROOT', dirname(dirname(__FILE__)));
     define('SITENAME', 'Clinic');
-    define('URLROOT', 'http://localhost/Physical_Tharepy_MVC_V.1.1/');
+    define('URLROOT', 'http://localhost/Physical Tharepy/');
     define('VIEWS_PATH', '../app/views/');
  ?>
 
