087d3130dc7b4916b881f41f72351fa593600bac
Ganna <gannaayman57@gmail.com>
2021-06-19 02:15:22 +0200

    receptioists main functions added

--- app/models/ReceptionistsAppointmentsModel.php
+++ app/models/ReceptionistsAppointmentsModel.php
@@ -4,5 +4,12 @@ class ReceptionistsAppointmentsModel extends model
 
     public $title = 'Receptionists';
 
+    function displayappointments()
+    {
+    $this->dbh->query("SELECT * FROM appointments");
+    $appointmnets = $this->dbh->resultSet();
+    return $appointments;
+    }
+
 }
 ?>
