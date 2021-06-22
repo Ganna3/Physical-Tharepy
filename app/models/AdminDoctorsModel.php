f9d3fe5752689a866add35336a32bc7455d3b98c
Ganna3 <gannaayman57@gmail.com>
2021-06-22 23:38:56 +0200

    admin add doctors

--- app/models/AdminDoctorsModel.php
+++ app/models/AdminDoctorsModel.php
@@ -1,7 +1,20 @@
 <?php
-require_once 'AdminModel.php';
-class AdminDoctorsModel extends AdminModel
+require_once 'DoctorModel.php';
+class AdminDoctorsModel extends DoctorModel
 {
-  
+    public function getDoctors()
+    {
+        $this->dbh->query("SELECT * FROM clinic_stuff WHERE Role ='Doctor' ");
+        $Doctors = $this->dbh->resultSet();
+        return $Doctors;
+    }
+
+    Public Function getDoctor_info($D_ID)
+    {
+    $this->dbh->query("SELECT * FROM doctors_data WHERE Doctor_ID =:D_ID ");
+    $this->dbh->bind(':D_ID', $D_ID);
+    $Doctor_info = $this->dbh->single();
+    return $Doctor_info;
+    }
 }
 ?>
